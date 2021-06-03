1dbe8269301eae09c4965bf76bb6cdb4f0f7d155
Andre Medeiros <32333858+armedeiros@users.noreply.github.com>
2021-06-02 21:09:30 -0300

    Update isis.py

--- isis.py
+++ isis.py
@@ -96,7 +96,7 @@ ids = voz.getProperty('voices') #vozes disponiveis
 
 rate = voz.getProperty('rate') #velocidade
 print("> Velocidade de fala..................: " + str(rate))
-voz.setProperty('rate', rate + 45) #ajusta velocidade da voz, buscando algo mais 'natural' +20
+voz.setProperty('rate', rate + 75) #ajusta velocidade da voz, buscando algo mais 'natural' +20
 
 volume = voz.getProperty('volume')
 print("> Volume de voz.......................: " + str(volume))
@@ -156,157 +156,160 @@ def cv():
 
 #Loop 'robótico'
 #inicia captura de audio
-with sr.Microphone() as s: 
+def main():
 
-	#limpa ruido
-	rec.adjust_for_ambient_noise (s)	
+    with sr.Microphone() as s: 
 
-	#loop de dialogo
-	while True: 
-		rec.adjust_for_ambient_noise(s)    
-		if (n == 0):
-			print("\n\n||||| INICIANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| ... [modo log] ...\n\n")
-			
-			voz.say("Oi Eu sou a PROTÓTIPO ÍSIS, como posso ajudar?")
-			voz.runAndWait()
-        
-        	##estrutura de ssegurança do bloco de execução, tratando exceções
-		try:
-			time = datetime.now()
+        #limpa ruido
+        rec.adjust_for_ambient_noise (s)	
 
-			print('ouvindo......: ' + str(time))
-			winsound.Beep(2200, 30)			
+        #loop de dialogo
+        while True: 
+            rec.adjust_for_ambient_noise(s)    
+            if (n == 0):
+                print("\n\n||||| INICIANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| ... [modo log] ...\n\n")
+                
+                voz.say("Oi Eu sou a PROTÓTIPO ÍSIS, como posso ajudar?")
+                voz.runAndWait()
             
-            #captura o audio
-			rec.adjust_for_ambient_noise(s)
+            ##estrutura de ssegurança do bloco de executação, tratando exceções
+            try:
+                time = datetime.now()
+
+                print('ouvindo......: ' + str(time))
+                winsound.Beep(2200, 30)			
+                
+                #captura o audio
+                rec.adjust_for_ambient_noise(s)
+                
+                audio = rec.listen(s) 
+                           
+                time = time.now()
+                
+                print('processando..: ' + str(time))
+                winsound.Beep(1000, 30)
+                #processa via google open
+                #call api para idioma pt-br/en 
+                #existem outras formas e calls, pode-se alterar caso necessário
+                #exemplo:
+                #lib sphinx para off-line, em pt-br ainda em testes/desenvolvimento
+                #audio_capt = rec.recognize_sphinx (audio)		
+                audio_capt = rec.recognize_google(audio, language='pt') 
+                
+                time = datetime.now()
+                print('respondendo..: ' + str(time) + '\n')
+                
+            except sr.UnknownValueError:
+                voz.say("Desculpe. Eu nao consegui compreender...")
+                voz.runAndWait()
+                n += 1
+                continue
+                
+            except sr.RequestError as e:
+                voz.say("Desculpe. Mas estamos sem conexao no momento...")
+                voz.runAndWait()
+                n += 1
+                continue
+                
+            except (KeyboardInterrupt, EOFError, SystemExit):
+                voz.say("Desculpe. Identifiquei um desligamento inesperado.")
+                voz.runAndWait()
+                print("\n\n||||| DESLIGANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| \n||||| Andre Medeiros ")
+                exit()	
+                
             
-			audio = rec.listen(s) 
-			           
-			time = time.now()
+            #imprime o que foi dito
+            print ("Ser humano diz ... ->   " + audio_capt + "\n")
             
-			print('processando..: ' + str(time))
-			winsound.Beep(1000, 30)
-			#processa via google open
-			#call api para idioma pt-br/en 
-            #existem outras formas e calls, pode-se alterar caso necessário
-            #exemplo:
-            #lib sphinx para off-line, em pt-br ainda em testes/desenvolvimento
-            #audio_capt = rec.recognize_sphinx (audio)		
-			audio_capt = rec.recognize_google(audio, language='pt') 
+            #upper pra facilitar os ifs
+            audio_capt = audio_capt.upper()
+                        
             
-			time = datetime.now()
-			print('respondendo..: ' + str(time) + '\n')
-			
-		except sr.UnknownValueError:
-			voz.say("Desculpe. Eu nao consegui compreender...")
-			voz.runAndWait()
-			n += 1
-			continue
-			
-		except sr.RequestError as e:
-			voz.say("Desculpe. Mas estamos sem conexao no momento...")
-			voz.runAndWait()
-			n += 1
-			continue
-			
-		except (KeyboardInterrupt, EOFError, SystemExit):
-			voz.say("Desculpe. Identifiquei um desligamento inesperado.")
-			voz.runAndWait()
-			print("\n\n||||| DESLIGANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| \n||||| Andre Medeiros ")
-			exit()	
-			
-		
-		#imprime o que foi dito
-		print ("Ser humano diz ... ->   " + audio_capt + "\n")
-		
-		#upper pra facilitar os ifs
-		audio_capt = audio_capt.upper()
-        			
-		
-        ###
-		###tratamento de ações executadas pelo protótipo, pode-se incluir quantas desejar 
-		###
-		
-		#verifica o que foi dito e executa ação em WINDOWS (você pode definir palavras chaves pra suas próprias ações e também outros sistemas operacionais)
-		if (audio_capt.find('DESLIGAR') >= 0): 
-			print("\n\n||||| DESLIGANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| \n||||| Andre Medeiros ") # + str(n)+ "\n")
-			voz.say("Desligando...")
-			voz.runAndWait()	
+            ###
+            ###tratamento de ações executadas pelo protótipo, pode-se incluir quantas desejar 
+            ###
+            
+            #verifica o que foi dito e executa ação em WINDOWS (você pode definir palavras chaves pra suas próprias ações e também outros sistemas operacionais)
+            if (audio_capt.find('DESLIGAR') >= 0): 
+                print("\n\n||||| DESLIGANDO ÍSIS - PROTÓTIPO COMPUTACIONAL MAKER ||||| \n||||| Andre Medeiros ") # + str(n)+ "\n")
+                voz.say("Desligando...")
+                voz.runAndWait()	
 
-			#fim dialogo
-			exit()  
-		
-		if (audio_capt.find('YOUTUBE') >= 0): 
-			busca = audio_capt[audio_capt.find('YOUTUBE')+8:]
-			print ("\nAção executada: Procurando videos com o tema... " + busca + "\n")
-			
-			#abre o caminho/path
-			os.startfile("https://www.youtube.com/results?search_query=" + busca)
-			action = 1
-			
-		if (audio_capt.find('OUTLOOK') >= 0): 
-			print ("\nAção executada: Abrindo e-mail...\n")
-			os.startfile("Outlook.exe")
-			action = 1
-		
-		if (audio_capt.find('GMAIL') >= 0): 
-			print ("\nAção executada: Abrindo Gmail...\n")
-			os.startfile("http://gmail.com")
-			action = 1
-			
-		if (audio_capt.find('CBN') >= 0): 
-			print ("\nAção executada: Abrindo radio on-line...\n")
-			os.startfile("http://cbn.globoradio.globo.com/servicos/estudio-ao-vivo/ESTUDIO-AO-VIVO.htm?praca=SP")
-			action = 1
+                #fim dialogo
+                exit()  
+            
+            if (audio_capt.find('YOUTUBE') >= 0): 
+                busca = audio_capt[audio_capt.find('YOUTUBE')+8:]
+                print ("\nAção executada: Procurando videos com o tema... " + busca + "\n")
+                
+                #abre o caminho/path
+                os.startfile("https://www.youtube.com/results?search_query=" + busca)
+                action = 1
+                
+            if (audio_capt.find('OUTLOOK') >= 0): 
+                print ("\nAção executada: Abrindo e-mail...\n")
+                os.startfile("Outlook.exe")
+                action = 1
+            
+            if (audio_capt.find('GMAIL') >= 0): 
+                print ("\nAção executada: Abrindo Gmail...\n")
+                os.startfile("http://gmail.com")
+                action = 1
+                
+            if (audio_capt.find('CBN') >= 0): 
+                print ("\nAção executada: Abrindo radio on-line...\n")
+                os.startfile("http://cbn.globoradio.globo.com/servicos/estudio-ao-vivo/ESTUDIO-AO-VIVO.htm?praca=SP")
+                action = 1
 
-		if (audio_capt.find('PESQUIS') >= 0): 
-			
-			busca = audio_capt[audio_capt.find('PESQUIS') + 9:]
-			print ("\nAção executada: Pesquisando no Google sobre... " + busca + "\n")
+            if (audio_capt.find('PESQUIS') >= 0): 
+                
+                busca = audio_capt[audio_capt.find('PESQUIS') + 9:]
+                print ("\nAção executada: Pesquisando no Google sobre... " + busca + "\n")
 
-			#abre o caminho invocando o browser padrao
-			os.startfile("https://www.google.com.br/search?hl=pt-BR&q=" + busca)
-			action = 1
-		
-		if (audio_capt.find('TEMPO') >= 0) or (audio_capt.find('CLIMA') >= 0): 
-			
-			print ("\nAção executada: Verificando as condições climáticas...\n" )	
-            #abre o caminho invocando o browser padrao
-			os.startfile("https://www.ipmetradar.com.br/")			
-			action = 1
-        
-		if (audio_capt.find('ATIVAR CAMERA') >= 0) or (audio_capt.find('ATIVAR CÂMERA') >= 0) or (audio_capt.find('ATIVAR A CÂMERA') >= 0): 
+                #abre o caminho invocando o browser padrao
+                os.startfile("https://www.google.com.br/search?hl=pt-BR&q=" + busca)
+                action = 1
+            
+            if (audio_capt.find('TEMPO') >= 0) or (audio_capt.find('CLIMA') >= 0): 
+                
+                print ("\nAção executada: Verificando as condições climáticas...\n" )	
+                #abre o caminho invocando o browser padrao
+                os.startfile("https://www.ipmetradar.com.br/")			
+                action = 1
+            
+            if (audio_capt.find('ATIVAR CAMERA') >= 0) or (audio_capt.find('ATIVAR CÂMERA') >= 0) or (audio_capt.find('ATIVAR A CÂMERA') >= 0): 
 
-			voz.say("Ativando câmera para identificar objetos")
-			voz.runAndWait()
-			
-			###inicia camera 
-			cv()
-			action = 1
-		
-		if (audio_capt.find('DIGITE') >= 0): 
-			repetir = audio_capt[audio_capt.find('DIGITE')+6:]
-			
-			print ("\nAção executada: Repetindo a frase ..." + repetir + "\n" )			
+                voz.say("Ativando câmera para identificar objetos")
+                voz.runAndWait()
+                
+                ###inicia camera 
+                cv()
+                action = 1
+            
+            if (audio_capt.find('DIGITE') >= 0): 
+                repetir = audio_capt[audio_capt.find('DIGITE')+6:]
+                
+                print ("\nAção executada: Repetindo a frase ..." + repetir + "\n" )			
 
-			### TESTE - automação com pywinauto - escrever o que foi dito pelo humano num arquivo .txt
-			app = Application().start("notepad.exe")			
+                ### TESTE - automação com pywinauto - escrever o que foi dito pelo humano num arquivo .txt
+                app = Application().start("notepad.exe")			
 
-			# 'digita' o texto capturado pelo audio
-			app.UntitledNotepad.Edit.type_keys(repetir, with_spaces = True)			
-			###------
-						
-			action = 1
+                # 'digita' o texto capturado pelo audio
+                app.UntitledNotepad.Edit.type_keys(repetir, with_spaces = True)			
+                ###------
+                            
+                action = 1
 
-			
-		if action == 0: # se nao for uma ação ela processa uma resposta 
-			#a robo responde (processo array com palavras já treinadas e ouvidas) - para reiniciar vocabulário, basta apagar arquivo dbsqlite
-			resposta = bot.get_response(audio_capt)
-			print ("ISIS responde ... ->   " + str(resposta) + "\n")
-			voz.say(resposta)
-			voz.runAndWait()				
+                
+            if action == 0: # se nao for uma ação ela processa uma resposta 
+                #a robo responde (processo array com palavras já treinadas e ouvidas) - para reiniciar vocabulário, basta apagar arquivo dbsqlite
+                resposta = bot.get_response(audio_capt)
+                print ("ISIS responde ... ->   " + str(resposta) + "\n")
+                voz.say(resposta)
+                voz.runAndWait()				
 
-		n += 1 #contador
-		action = 0 #verifica acoes executadas
+            n += 1 #contador
+            action = 0 #verifica acoes executadas
 
-
+if __name__ == "__main__":
+    main()
